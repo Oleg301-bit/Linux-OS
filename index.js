7324fffa8e721cd63b4288e9837bc0aa2264bd65
Oleg <olegmaslakov47@gmail.com>
2025-03-02 23:39:06 +0200

    Update js code

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 'use strict';
-let gainFigure = Number(prompt("Please enter figure from 0 to 7"));
-
-  switch (gainFigure) {
-    case 0:
-      console.log(`You put ${gainFigure} . Binary number:000. Access mode:---`);
-      break;
-    case 1:
-      console.log(`You put ${gainFigure} . Binary number:001. Access mode:--x`);
-      break;
-    case 2:
-      console.log(`You put ${gainFigure} . Binary number:010. Access mode:-w-`);
-      break;
-    case 3:
-      console.log(`You put ${gainFigure} . Binary number:011. Access mode:-wx`);
-      break;
-    case 4:
-      console.log(`You put ${gainFigure} . Binary number:100. Access mode:r--`);
-      break;
-    case 5:
-      console.log(`You put ${gainFigure} . Binary number:101. Access mode:r-x`);
-      break;
-    case 6:
-      console.log(`You put ${gainFigure} . Binary number:110. Access mode:rw-`);
-      break;
-    case 7:
-      console.log(`You put ${gainFigure} . Binary number:111. Access mode:rwx`);
-      break;
-    default:
-      console.log("This is wrong number");
-  }
-
-
+let gainFigure = Number(prompt('Please enter figure from 0 to 7'));
+switch (gainFigure) {
+  case 0:
+    console.log(`You put ${gainFigure} . Binary number:000. Access mode:---`);
+    break;
+  case 1:
+    console.log(`You put ${gainFigure} . Binary number:001. Access mode:--x`);
+    break;
+  case 2:
+    console.log(`You put ${gainFigure} . Binary number:010. Access mode:-w-`);
+    break;
+  case 3:
+    console.log(`You put ${gainFigure} . Binary number:011. Access mode:-wx`);
+    break;
+  case 4:
+    console.log(`You put ${gainFigure} . Binary number:100. Access mode:r--`);
+    break;
+  case 5:
+    console.log(`You put ${gainFigure} . Binary number:101. Access mode:r-x`);
+    break;
+  case 6:
+    console.log(`You put ${gainFigure} . Binary number:110. Access mode:rw-`);
+    break;
+  case 7:
+    console.log(`You put ${gainFigure} . Binary number:111. Access mode:rwx`);
+    break;
+  default:
+    console.log('This is wrong number');
+}
